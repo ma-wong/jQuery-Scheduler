1a6748dac3484a16c1426a7928a333bcad096da5
Morgan <morganw118@gmail.com>
2020-11-09 21:03:23 -0800

    added color coding with jquery

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,6 @@
 $('#currentDay').text(moment().format('LLLL'));
+var currentTime = moment().hour();
+
 
 // Save event content in local storage
 $('.save-btn').click(function() {
@@ -58,11 +60,154 @@ renderEvents();
 // Clear events from scheduler
 
 // $('.clear-btn').click(function() {
+//     var timeSlot = $(this).attr('value');
 
 // })
 
+// var hours = [9, 10, 11, 12, 13, 14, 15, 16, 17]
+// for (elem of hours) {
+//     if (currentTime === elem) {
+//         $('.form-control').css('background-color', 'red');
+//         console.log('current time');
+//     }
+//     else if (currentTime > elem) {
+//         $('.form-control').css('background-color', 'grey');
+//         console.log('already passed');
+//     }
+//     else if (currentTime < elem) {
+//         $('.form-control').css('background-color', 'green');
+//         console.log('coming up');
+//     }
+// }
+
+// function checkCurrentTime()
 
 
+// if (currentTime.isBetween(moment({hour: 14}), moment({hour: 15}))) {
+//     $('.form-control').css('background-color', 'red');
+//     console.log('hello');
+// }
 
+function checkCurrentTime() {
+    // 9am
+    if (currentTime === 9) {
+        $('#nine-am').css('background-color', 'lightcoral');
+        console.log('current time');
+    }
+    else if (currentTime > 9) {
+        $('#nine-am').css('background-color', 'grey');
+        console.log('already passed');
+    }
+    else if (currentTime < 9) {
+        $('#nine-am').css('background-color', 'lightgreen');
+        console.log('coming up');
+    }
+    // 10am
+    if (currentTime === 10) {
+        $('#ten-am').css('background-color', 'lightcoral');
+        console.log('current time');
+    }
+    else if (currentTime > 10) {
+        $('#ten-am').css('background-color', 'grey');
+        console.log('already passed');
+    }
+    else if (currentTime < 10) {
+        $('#ten-am').css('background-color', 'lightgreen');
+        console.log('coming up');
+    }
+    // 11am
+    if (currentTime === 11) {
+        $('#eleven-am').css('background-color', 'lightcoral');
+        console.log('current time');
+    }
+    else if (currentTime > 11) {
+        $('#eleven-am').css('background-color', 'grey');
+        console.log('already passed');
+    }
+    else if (currentTime < 11) {
+        $('#eleven-am').css('background-color', 'lightgreen');
+        console.log('coming up');
+    }
+    // 12pm
+    if (currentTime === 12) {
+        $('#twelve-pm').css('background-color', 'lightcoral');
+        console.log('current time');
+    }
+    else if (currentTime > 12) {
+        $('#twelve-pm').css('background-color', 'grey');
+        console.log('already passed');
+    }
+    else if (currentTime < 12) {
+        $('#twelve-pm').css('background-color', 'lightgreen');
+        console.log('coming up');
+    }
+    // 1pm
+    if (currentTime === 13) {
+        $('#one-pm').css('background-color', 'lightcoral');
+        console.log('current time');
+    }
+    else if (currentTime > 13) {
+        $('#one-pm').css('background-color', 'grey');
+        console.log('already passed');
+    }
+    else if (currentTime < 13) {
+        $('#one-pm').css('background-color', 'lightgreen');
+        console.log('coming up');
+    }
+    // 2pm
+    if (currentTime === 14) {
+        $('#two-pm').css('background-color', 'lightcoral');
+        console.log('current time');
+    }
+    else if (currentTime > 14) {
+        $('#two-pm').css('background-color', 'grey');
+        console.log('already passed');
+    }
+    else if (currentTime < 14) {
+        $('#two-pm').css('background-color', 'lightgreen');
+        console.log('coming up');
+    }
+    // 3pm
+    if (currentTime === 15) {
+        $('#three-pm').css('background-color', 'lightcoral');
+        console.log('current time');
+    }
+    else if (currentTime > 15) {
+        $('#three-pm').css('background-color', 'grey');
+        console.log('already passed');
+    }
+    else if (currentTime < 15) {
+        $('#three-pm').css('background-color', 'lightgreen');
+        console.log('coming up');
+    }
+    // 4pm
+    if (currentTime === 16) {
+        $('#four-pm').css('background-color', 'lightcoral');
+        console.log('current time');
+    }
+    else if (currentTime > 16) {
+        $('#four-pm').css('background-color', 'grey');
+        console.log('already passed');
+    }
+    else if (currentTime < 16) {
+        $('#four-pm').css('background-color', 'lightgreen');
+        console.log('coming up');
+    }
+    // 5pm
+    if (currentTime === 17) {
+        $('#five-pm').css('background-color', 'lightcoral');
+        console.log('current time');
+    }
+    else if (currentTime > 17) {
+        $('#five-pm').css('background-color', 'grey');
+        console.log('already passed');
+    }
+    else if (currentTime < 17) {
+        $('#five-pm').css('background-color', 'lightgreen');
+        console.log('coming up');
+    }
+}
+
+checkCurrentTime();
 
 
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # jQuery-Scheduler
+
+## Description
+This repo consists of the code used to develop and run a functioning 'Work-Day Scheduler'. Designed to keep past, present, and upcoming events organized, this scheduler allows the user to save their work day events in designated time slots and color codes them in regards to the current time.
+
+### Technologies
+Project is created with:
+- html
+- css
+- javascript/jQuery
+
+### Features
+
+## Functionality 
+
